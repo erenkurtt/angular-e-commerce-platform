371bebc6c8633eb9866dbf6d1fa9f30f298312cf
Eren Kurt <erenkurt@Erens-Mac-mini.local>
2024-07-21 15:59:53 +0300

    login page ui has been completed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
 
+  onSubmit() {
+      // Handle login logic here
+      console.log('Login form submitted', this.email, this.password);
+  }
 }
